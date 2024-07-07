d172380495fae8bd42ec436a1e0d9d34c559ff56
Sergey <116721624+monocvet@users.noreply.github.com>
2024-07-07 18:43:10 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Vlasov");
+        System.out.println("04.04.1981");
+    }
+}
